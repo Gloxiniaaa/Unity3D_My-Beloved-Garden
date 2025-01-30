885ef5755025210c57065bddd5a80e75fa090ac5
Gloxiniaaa <hnm142758@gamil.com>
2025-01-30 11:26:04 +0700

    refactor: reame variable based on convention in inputreader

--- Assets/Scripts/Input/InputReader.cs
+++ Assets/Scripts/Input/InputReader.cs
@@ -8,11 +8,11 @@ namespace GameInput
     public class InputReaderSO : ScriptableObject, GameInput.IPlayerActions
     {
         private GameInput _gameInput;
-        private InputAction touchPositionAction;
-        private readonly float swipeDistanceThreshold = 60f;
-        private readonly float timeThreshold = 0.5f;
-        private Vector2 startTouchPosition;
-        private float touchStartTime;
+        private InputAction _touchPositionAction;
+        private readonly float _swipeDistanceThreshold = 60f;
+        private readonly float _timeThreshold = 0.5f;
+        private Vector2 _startTouchPosition;
+        private float _touchStartTime;
 
         public event UnityAction<Vector2> Move = delegate { };
         [SerializeField] private VoidEventChannelSO _undoChannel;
@@ -24,7 +24,7 @@ namespace GameInput
             _gameInput = new GameInput();
             _gameInput.Player.SetCallbacks(this);
             _gameInput.Player.Enable();
-            touchPositionAction = _gameInput.Player.PrimaryPosition;
+            _touchPositionAction = _gameInput.Player.PrimaryPosition;
         }
 
         public void OnMove(InputAction.CallbackContext context) //WASD
@@ -39,20 +39,20 @@ namespace GameInput
         {
             if (context.phase == InputActionPhase.Started)
             {
-                startTouchPosition = touchPositionAction.ReadValue<Vector2>();
-                touchStartTime = Time.time;
+                _startTouchPosition = _touchPositionAction.ReadValue<Vector2>();
+                _touchStartTime = Time.time;
             }
             if (context.phase == InputActionPhase.Canceled)
             {
-                if (Time.time - touchStartTime > timeThreshold)
+                if (Time.time - _touchStartTime > _timeThreshold)
                 {
                     return; // Swipe too slow, ignore
                 }
 
-                Vector2 endTouchPosition = touchPositionAction.ReadValue<Vector2>();
-                Vector2 swipeDelta = endTouchPosition - startTouchPosition;
+                Vector2 endTouchPosition = _touchPositionAction.ReadValue<Vector2>();
+                Vector2 swipeDelta = endTouchPosition - _startTouchPosition;
 
-                if (swipeDelta.magnitude > swipeDistanceThreshold)
+                if (swipeDelta.magnitude > _swipeDistanceThreshold)
                 {
                     Move.Invoke(GetSwipeDirection(swipeDelta));
                 }
