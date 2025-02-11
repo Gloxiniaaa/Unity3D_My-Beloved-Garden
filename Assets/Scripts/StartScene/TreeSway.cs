aa27807693876ac1f5bafc1d7f3aec619bc90101
Gloxiniaaa <hnm142758@gamil.com>
2025-02-11 21:20:25 +0700

    setup startscene's geometry

---
+++ Assets/Scripts/StartScene/TreeSway.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class TreeSway : MonoBehaviour
+{
+    [SerializeField][Range(1, 5)] private float swayAngle = 5f;  // Maximum sway angle (degrees)
+    [SerializeField][Range(0, 2)] private float swayDuration = 1f;  // Time to complete one sway cycle
+
+    private void Start()
+    {
+        StartSwaying();
+    }
+
+    private void StartSwaying()
+    {
+        transform.DOLocalRotate(new Vector3(0, 0, swayAngle), swayDuration)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+}
