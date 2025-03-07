db10c81bfea5205f3095a6bb575aa6d26a9c0a40
Gloxiniaaa <hnm142758@gamil.com>
2025-03-07 10:40:45 +0700

    fix: dotween animation for button

--- Assets/Scripts/UI/ItemButton.cs
+++ Assets/Scripts/UI/ItemButton.cs
@@ -10,8 +10,10 @@ public class ItemButton : MonoBehaviour
 
     public void OnClick()
     {
+        if (transform.localScale != Vector3.one) // act as a cooldown, preven player from spamming
+            return;
         _useItemChannel.RaiseEvent();
         _uiSfxChannel.RaiseEvent(_clickSfx);
-        transform.DOScale(Vector3.one * 1.1f, 0.1f).SetLoops(2, LoopType.Yoyo);
+        transform.DOScale(Vector3.one * 1.1f, 0.1f).SetLoops(2, LoopType.Yoyo).OnComplete(() => { transform.localScale = Vector3.one; });
     }
 }
